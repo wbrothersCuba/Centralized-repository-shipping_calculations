71ad2d7b56af1d946995f490b445a96fb36cdbc6
wbrothersCuba <yanet.wilmar@gmail.com>
2026-01-11 18:42:59 -0500

    adding new changes to Shipping_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,5 +9,5 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
- # Here is a new update by wbrothersCuba
-
+# Here is a new update by wbrothersCuba
+# Here is another update by wbrothersCuba
